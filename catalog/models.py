bc15bd9c5d6ce14142500f101936eee3ec29937e
61-shahrin <shahrin999910@gmail.com>
2025-10-13 19:58:05 +0600

    Add files via upload

---
+++ catalog/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=140, unique=True, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="category_images/", blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    @property
+    def product_count(self):
+        return self.products.filter(is_active=True).count()
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=180)
+    slug = models.SlugField(max_length=220, blank=True, unique=True)
+    short_description = models.CharField(max_length=250, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base = slugify(self.title)
+            # avoid duplicates while seeding
+            self.slug = f"{base}"
+        super().save(*args, **kwargs)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+
+    @property
+    def discount_percentage(self):
+        if self.old_price and self.old_price > self.price:
+            return round(((self.old_price - self.price) / self.old_price) * 100)
+        return 0
